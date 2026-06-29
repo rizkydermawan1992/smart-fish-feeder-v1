33eff6402fa55460b35b63101ead28f6c9f989a4
rizkydermawan1992 <rizkydermawan.arduino@gmail.com>
2026-06-29 23:50:26 +0800

    mqtt config api

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const buttonStyle = "bg-cyan-600 text-white p-3 rounded-xl w-full";
 let USE_DUMMY_PH = true;
 let mqttConnected = false;
 
+// MQTT GLOBAL
+let MQTT_CONFIG = null;
+let client = null;
+
 // apply style
 document.querySelectorAll(".input").forEach(e => {
     e.classList.add(...inputStyle.split(" "));
@@ -99,8 +103,7 @@ function updatePH(ph) {
     chart.update();
 }
 
-// start dummy
-startDummy();
+
 
 
 // ================= FEED COMMAND =================
@@ -124,76 +127,136 @@ function feedNow() {
 
 
 // ================= MQTT CONFIG =================
-const MQTT_BROKER = "broker.emqx.io";
-const MQTT_PORT = "8084";
+const MQTT_CONFIG_API = "https://n8n-35yaee339qxb.jkt6.sumopod.my.id/webhook/9a1aef5e-bc89-4204-950f-53fd634c20e5";
+//const MQTT_CONFIG_API = "https://n8n-35yaee339qxb.jkt6.sumopod.my.id/webhook-test/9a1aef5e-bc89-4204-950f-53fd634c20e5"
 
-const MQTT_CONFIG = {
-    url: `wss://${MQTT_BROKER}:${MQTT_PORT}/mqtt`,
-    clientId: "web_" + Math.random().toString(16).substr(2, 8),
+async function loadMQTTConfig() {
+    try {
+        const response = await fetch(MQTT_CONFIG_API, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                request: "mqtt_config"
+            })
+        });
+        const config = await response.json();
 
-    topicPh: "fish/ph",
-    topicStatus: "fish/status",
-    topicFeed: "fish/feed"
-};
+        MQTT_CONFIG = {
+            url: `wss://${config.broker}:${config.port}/mqtt`,
+            clientId: "web_" + Math.random().toString(16).substr(2, 8),
+            topicSensor: config.sensor_topic,
+            topicStatus: config.status_topic
+        };
 
-const client = mqtt.connect(MQTT_CONFIG.url, {
-    clientId: MQTT_CONFIG.clientId,
-    clean: true,
-    connectTimeout: 4000,
-});
+        document.getElementById("mqttBroker").value = config.broker;
+        document.getElementById("mqttPort").value = config.port;
+        document.getElementById("sensorTopic").value = config.sensor_topic;
+        document.getElementById("statusTopic").value = config.status_topic;
 
-
-client.on("connect", () => {
-    console.log("MQTT Connected");
-    mqttConnected = true;
-
-    client.subscribe([
-        MQTT_CONFIG.topicPh,
-        MQTT_CONFIG.topicStatus
-    ]);
-
-    const el = document.getElementById("mqttStatus");
-
-    if (el) {
-        el.innerHTML = "● Connected";
-        el.classList.remove(
-            "text-red-700",
-            "bg-red-100",
-            "border-red-200"
-        );
-        el.classList.add(
-            "text-green-700",
-            "bg-green-100",
-            "border-green-200"
-        );
+        console.log("MQTT CONFIG", MQTT_CONFIG);
+        connectMQTT();
     }
-
-});
-
-client.on("disconnect", () => {
-    mqttConnected = false;
-});
-
-client.on("message", (topic, message) => {
-    const data = message.toString();
-
-    // ================= PH SENSOR =================
-    if (topic === MQTT_CONFIG.topicPh) {
-        const ph = parseFloat(data);
-        updatePH(ph);
+    catch (error) {
+        console.log("Config error:", error);
     }
-
-    // ================= ESP32 STATUS =================
-    if (topic === MQTT_CONFIG.topicStatus) {
-        const espStatus = document.getElementById("espStatus");
-        espStatus.innerHTML = "● Online";
-        espStatus.className = "bg-green-100 text-green-700 px-3 py-1 rounded-full border border-green-200";
-
-        console.log("ESP32 Status:", data);
-    }
-});
-
-function saveMqttConfig() {
-    let broker = document.getElementById("mqttBroker").value;
-    console.log(broker);
 }
+
+
+function connectMQTT() {
+    client = mqtt.connect(
+        MQTT_CONFIG.url,
+        {
+            clientId: MQTT_CONFIG.clientId,
+            clean: true,
+            connectTimeout: 4000
+        }
+    );
+
+    client.on("connect", () => {
+        console.log("MQTT Connected");
+
+        mqttConnected = true;
+        client.subscribe([MQTT_CONFIG.topicSensor, MQTT_CONFIG.topicStatus]);
+
+        const el = document.getElementById("mqttStatus");
+
+        if (el) {
+            el.innerHTML = "● Connected";
+            el.className = "bg-green-100 text-green-700 px-3 py-1 rounded-full border border-green-200";
+        }
+    });
+
+    // DISCONNECT
+    client.on("disconnect", () => {
+        mqttConnected = false;
+        const el = document.getElementById("mqttStatus");
+
+        if (el) {
+            el.innerHTML = "● Disconnected";
+            el.className = "bg-red-100 text-red-700 px-3 py-1 rounded-full border border-red-200";
+        }
+
+    });
+
+    // MESSAGE
+    client.on("message", (topic, message) => {
+        const data = message.toString();
+        // PH SENSOR
+        if (topic === MQTT_CONFIG.topicSensor) {
+            const ph = parseFloat(data);
+            updatePH(ph);
+        }
+        // ESP32 STATUS
+        if (topic === MQTT_CONFIG.topicStatus) {
+            const espStatus = document.getElementById("espStatus");
+            espStatus.innerHTML = "● Online";
+            espStatus.className = "bg-green-100 text-green-700 px-3 py-1 rounded-full border border-green-200";
+
+            console.log("ESP32 Status:", data);
+        }
+    });
+}
+
+
+async function saveMqttConfig() {
+    let mqtt_broker = document.getElementById("mqttBroker").value;
+    let mqtt_port = document.getElementById("mqttPort").value;
+    let sensor_topic = document.getElementById("sensorTopic").value;
+    let status_topic = document.getElementById("statusTopic").value;
+
+    const payload = {
+        broker: mqtt_broker,
+        port: Number(mqtt_port),
+        sensor_topic: sensor_topic,
+        status_topic: status_topic
+    };
+
+    console.log("Data dikirim:", payload);
+
+    try {
+        const response = await fetch(MQTT_CONFIG_API, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        });
+
+        const result = await response.json();
+
+        console.log("Response API:", result);
+        alert("MQTT Config berhasil disimpan");
+    }
+
+    catch (error) {
+        console.error("Gagal kirim config:", error);
+        alert("Gagal menyimpan MQTT Config");
+    }
+}
+
+// start dummy
+startDummy();
+
+loadMQTTConfig();
